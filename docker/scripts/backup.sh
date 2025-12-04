69c351935250f64339d96c6e00924927494b056b
nasruladitri <nasruladitri2@gmail.com>
2025-12-04 11:39:03 +0700

    delete all

--- docker/scripts/backup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Backing up data..."
-tar -czf backup_$(date +%Y%m%d).tar.gz docker/data
-echo "Backup complete!"
--- docker/scripts/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Setting up environment..."
-cp .env.example .env
-docker-compose -f docker/compose/docker-compose.yml up -d --build
-echo "Setup complete!"
